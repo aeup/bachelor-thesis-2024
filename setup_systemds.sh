dc42f7333800d6ae1be65416f28464403932bc13
Anton Pötzsch <60671371+aeup@users.noreply.github.com>
2024-09-11 02:22:49 +0200

    add setup systemds script

---
+++ setup_systemds.sh
@@ -0,0 +1,24 @@
+echo "Installing dependencies..."
+sudo apt update
+sudo apt install -y git openjdk-11-jdk maven r-base
+
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+
+echo "Setting up SystemDS..."
+rm -rf dependencies
+mkdir -p dependencies
+cd dependencies
+git clone git@github.com:apache/systemds.git
+
+cd systemds
+export SYSTEMDS_ROOT=$(pwd)
+export PATH=$SYSTEMDS_ROOT/bin:$PATH
+
+echo "Installing test dependencies..."
+Rscript ./src/test/scripts/installDependencies.R
+
+echo "Building SystemDS..."
+mvn package -P distribution
+
+echo "SystemDS is set up."
