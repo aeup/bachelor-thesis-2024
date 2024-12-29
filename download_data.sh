050bd89ec5c9f8dfdaf63621608217f01dc7726b
Anton Pötzsch <60671371+aeup@users.noreply.github.com>
2024-12-29 12:33:06 +0100

    updated mem plot over time

--- download_data.sh
+++ download_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# this file is based on https://github.com/damslab/reproducibility/blob/master/sigmod2021-sliceline-p218/run3DownloadData.sh
+# this file based on https://github.com/damslab/reproducibility/blob/master/sigmod2021-sliceline-p218/run3DownloadData.sh
 
 export SYSTEMDS_ROOT=$(pwd)/dependencies/systemds
 export PATH=$SYSTEMDS_ROOT/bin:$PATH
@@ -14,11 +14,6 @@ sed -i '$d' data/Adult/Adult.csv;
 
 systemds -f dataprep/dataprepAdult.dml -exec singlenode -stats -nvargs FILE="./data/Adult/Adult.csv" OUTPUTDIR="./data/Adult/"
 
-## Adult Short
-#mkdir -p data/Adult/short
-
-#systemds -f dataprep/shortAdult.dml -stats -nvargs DIR="./data/Adult/" LENGTH=10000
-
 # Covtype
 mkdir -p data/Covtype
 curl https://archive.ics.uci.edu/ml/machine-learning-databases/covtype/covtype.data.gz -o ./data/Covtype/covtype.data.gz;
@@ -27,10 +22,6 @@ mv ./data/Covtype/covtype.data ./data/Covtype/Covtype.csv;
 
 systemds -f dataprep/dataprepCovtype.dml -exec singlenode -stats -nvargs FILE="./data/Covtype/Covtype.csv" OUTPUTDIR="./data/Covtype/"
 
-## Covtype Short
-#mkdir -p data/Covtype/short
-
-#systemds -f dataprep/shortCovtype.dml -nvargs DIR="./data/Covtype/" LENGTH=200
 
 # US Census
 mkdir -p data/USCensus
@@ -41,8 +32,3 @@ mv ./data/USCensus/USCensus1990.data.txt ./data/USCensus/USCensus.csv
 mv ./data/USCensus/USCensus1990.attributes.txt ./data/USCensus/USCensus.attributes.csv
 
 systemds -f dataprep/dataprepUSCensus.dml -exec singlenode -stats -nvargs FILE="./data/USCensus/USCensus.csv" OUTPUTDIR="./data/USCensus/"
-
-## USCensus Short
-#mkdir -p data/USCensus/short
-
-#systemds -f dataprep/shortUSCensus.dml -nvargs DIR="./data/USCensus/" LENGTH=5000
--- scripts/mem_bench_plot_over_time.py
+++ scripts/mem_bench_plot_over_time.py
@@ -2,47 +2,31 @@ import csv
 
 import matplotlib.pyplot as plt
 
-file_name_mapping_array = [
-    ("breadth-first", "Breadth-First"),
-    ("depth-first", "Depth-First"),
-    ("min-intermediate", "Min Intermediate")
-]
-
 testcases = [
-    #("autoencoder","Autoencoder"),
-    #("decisionTree","decisionTree"),
-    #("kmeans", "kMeans"),
-    #("lmCG", "lmCG"),
-    #("multiLogReg", "multiLogReg"),
-    #("pca", "PCA"),
-    #("pnmf", "PNMF"),
-    ("slicefinder_Adult", "slicefinder Adult Dataset"),
-    ("slicefinder_Covtype", "slicefinder Covtype Dataset"),
-    ("slicefinder_USCensus", "slicefinder US Census Dataset"),
-    #("stratstats", "stratstats")
+    ('MEMORY_resource-aware_multiLogReg_Adult.csv', 'MEMORY_OVER_TIME_multiLogReg_Adult.png'),
+    ('MEMORY_resource-aware_stratstats_Adult.csv', 'MEMORY_OVER_TIME_stratstats.png')
 ]
 
+for (file_name, output_file_name) in testcases:
+    with open('temp/'+file_name, newline='') as f:
+        file_array = []
+        time_array = []
+        memory_array = []
 
-for (file_name, display_name) in file_name_mapping_array:
-    for (testcase_file, (testcase_name)) in testcases:
-        with open('temp/MEMORY_'+file_name+'_'+testcase_file+'.csv', newline='') as f:
-            file_array = []
-            time_array = []
-            memory_array = []
+        reader = csv.reader(f, delimiter=";")
+        for row in reader:
+            file_array.append(row)
 
-            reader = csv.reader(f, delimiter=";")
-            for row in reader:
-                file_array.append(row)
+        for j in range(len(file_array[0])):
+            # removes the data pairs that are markers and no real data
+            if not(j == 0 or j == 1 or j == (len(file_array[0]) - 1)):
+                time_array.append(int(file_array[0][j]) / 1000)
+                memory_array.append(int(file_array[1][j]))
 
-            for i in range(int(len(file_array) / 2)):
-                for j in range(len(file_array[i*2])):
-                    if not(j == 0 or j == 1 or j == (len(file_array[i*2]) - 1)):
-                        time_array.append(int(file_array[i*2][j]))
-                        memory_array.append(int(file_array[i*2+1][j]))
-
-            fig, ax = plt.subplots()
-            ax.scatter(time_array, memory_array)#, c='b')
-            # ax.scatter(time_array[1::2], memory_array[1::2], c='r')
-            ax.set_xlabel("Time in ms")
-            ax.set_ylabel("Memory Consumption in MB")
-            plt.savefig('results/MEMORY_OVER_TIME_'+file_name+'_'+testcase_file+'.png')
+        fig, ax = plt.subplots()
+        ax.plot(time_array, memory_array, color="black")
+        ax.set_ylim(ymin=0)
+        ax.set_xlim(xmin=0)
+        ax.set_xlabel("Time in s")
+        ax.set_ylabel("Memory Consumption in MB")
+        plt.savefig('results/'+output_file_name)
